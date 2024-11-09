213fbd2d87538589f15ef9b0d02c1b04d1e1a808
Yujin Kim <yujii22001@gmail.com>
2024-11-09 20:32:09 +0900

    feat: useReducer로 로직 분리하기

--- api-test/src/User.jsx
+++ api-test/src/User.jsx
@@ -1,44 +1,77 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import axios from "axios";
-import usersData from "./usersData";
+//import usersData from "./usersData";
+
+//reducer함수는 User 밖에 선언
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
 
 const User = () => {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // useReducer 의 장점
+  //  -> useState를 사용하지 않아도 된다.
+  //  -> 다른 곳에서도 쉽게 재사용할 수 있다.
+  //  -> setState() 대신에 dispatch() 로 사용한다.
+
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
+
+  const fetchUsers = async () => {
+    dispatch({ type: "LOADING" });
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      dispatch({ type: "SUCCESS", data: res.data });
+    } catch (e) {
+      dispatch({ type: "ERROR", error: e });
+    }
+  };
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        setError(null);
-        setUsers(null);
-        setLoading(true);
-        const res = await axios.get(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setUsers(res.data);
-        // setUsers(usersData);
-        // 이 폴더에 데이터 파일이 있으면 axios로 api를 호출하지 않아도 된다.
-      } catch (e) {
-        setError(e);
-      }
-      setLoading(false);
-    };
     fetchUsers();
   }, []);
+
+  // reducer에서 쓴 변수들 할당해주기
+  const { loading, data: users, error } = state;
 
   if (loading) return <div>로딩 중...</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
   if (!users) return null;
 
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.username} ({user.name})
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}>불러오기</button>
+    </>
   );
 };
 
